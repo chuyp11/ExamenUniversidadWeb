6ca3e68820199d4aedcd862d5fb7e63facae1a7e
miguel.riverar <miguel.riverar@uienl.edu.mx>
2018-04-09 12:48:33 -0500

    09/04/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // materialAngular componentes
 import { MatButtonModule,
+  MatCardModule,
   MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
   MatSidenavModule,
+  MatTableModule,
   MatToolbarModule
   } from '@angular/material';
 // materialAngular animaciones
@@ -15,19 +19,27 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { LecturaMateriasComponent } from './componentes/administrar-materias/lectura-materias/lectura-materias.component';
+import { AltaMateriasComponent } from './componentes/administrar-materias/alta-materias/alta-materias.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    PiePaginaComponent
+    PiePaginaComponent,
+    LecturaMateriasComponent,
+    AltaMateriasComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatSidenavModule,
+    MatTableModule,
     MatToolbarModule,
     BrowserAnimationsModule
   ],
---
+++ src/app/componentes/administrar-materias/alta-materias/alta-materias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-materias',
+  templateUrl: './alta-materias.component.html',
+  styleUrls: ['./alta-materias.component.css']
+})
+export class AltaMateriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
